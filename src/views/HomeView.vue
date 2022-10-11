0231c284ff3ff821a8f76a85084ee492363fe004
rpradana <rpradana@dgitsystems.com>
2022-10-11 23:27:55 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+   <NavBar></NavBar>
+   <Content></Content>
+</template>
+
+<script setup>
+import NavBar from '@/components/ui/NavBar.vue';
+import Content from '@/components/ui/Content.vue';
+</script>
